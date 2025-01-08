902593c1efc067bf3ff12b3890ec9afdf44e860e
zhenye234 <91582280+zhenye234@users.noreply.github.com>
2025-01-08 23:34:47 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-Single VQ X-Codec, 50 tokens per second, with multilingual speech semantic support and high quality audio reconstruction ( sim 0.82 on librispeech-test-clean).
+# X-Codec-2.0
+SingleX-Codec
+
+
+## Setup
+Code is tested on `python3.9`
+
+Please follow the following steps to setup your environment
+1. Clone this repo
+2. conda create --name xcodec2 python=3.9 
+3. conda activate xcodec2  
+2. `pip install -r requirements.txt`
+3. Download the pretrained checkpoint by
+```bash
+wget  
+```
+
+## Inference
+```bash
+python inference.py  
+```
+The above cmd reconstruct all `.wav` files under the input directory and write the results to the output directory using the checkpoint.
+
+BigCodec extracts a single token to represent each frame of the utterance. Refer to `inference.py` to find how to get the code.
+
+## Train
+To train a BigCodec, firstly you have to prepare your data 
+
+1. Make a file list by:
+```bash
+python get_tsv.py
+```
+
+2. Train a X-Codec-2.0 with the default setting by:
+
+```bash
+python train.py log_dir=/path/to/log_dir
+```
+
+## Large scaling training and code extracting:
+
+Training
+```bash
+Sbatch train_slurm.sh
+```
+
+Code extracting
+```bash
+Sbatch large_scale_save_code.sh
+```
+
+Codec will save in output folder with the same subfolder structure for audio file.
+
+
+ 
---
+++ inference.py
@@ -0,0 +1,142 @@
+import os
+import librosa
+import torch
+import torch.nn.functional as F
+import numpy as np
+import soundfile as sf
+from glob import glob
+from tqdm import tqdm
+from os.path import basename, join, exists
+from vq.codec_encoder import CodecEncoder_Transformer
+# from vq.codec_decoder import CodecDecoder
+from vq.codec_decoder_vocos import CodecDecoderVocos
+from argparse import ArgumentParser
+from time import time
+from transformers import  AutoModel
+import torch.nn as nn
+from vq.module import SemanticDecoder,SemanticEncoder
+from transformers import AutoFeatureExtractor, Wav2Vec2BertModel
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--input-dir', type=str, default='test_audio/input_test')
+    parser.add_argument('--ckpt', type=str, default='ckpt/epoch=4-step=1400000.ckpt')
+    parser.add_argument('--output-dir',   type=str, default='test_audio/output_test')
+             
+    args = parser.parse_args()
+    sr = 16000
+
+    print(f'Load codec ckpt from {args.ckpt}')
+    ckpt = torch.load(args.ckpt, map_location='cpu')
+    ckpt=ckpt['state_dict']
+
+    state_dict = ckpt
+    from collections import OrderedDict
+    # 步骤 2：提取并过滤 'codec_enc' 和 'generator' 部分
+    filtered_state_dict_codec = OrderedDict()
+    filtered_state_dict_semantic_encoder = OrderedDict()
+    filtered_state_dict_gen = OrderedDict()
+    filtered_state_dict_fc_post_a = OrderedDict()
+    filtered_state_dict_fc_prior = OrderedDict()
+    for key, value in state_dict.items():
+        if key.startswith('CodecEnc.'):
+            # 去掉 'codec_enc.' 前缀，以匹配 CodecEncoder 的参数名
+            new_key = key[len('CodecEnc.'):]
+            filtered_state_dict_codec[new_key] = value
+        elif key.startswith('generator.'):
+            # 去掉 'generator.' 前缀，以匹配 CodecDecoder 的参数名
+            new_key = key[len('generator.'):]
+            filtered_state_dict_gen[new_key] = value
+        elif key.startswith('fc_post_a.'):
+            # 去掉 'generator.' 前缀，以匹配 CodecDecoder 的参数名
+            new_key = key[len('fc_post_a.'):]
+            filtered_state_dict_fc_post_a[new_key] = value
+        elif key.startswith('SemanticEncoder_module.'):
+            # 去掉 'generator.' 前缀，以匹配 CodecDecoder 的参数名
+            new_key = key[len('SemanticEncoder_module.'):]
+            filtered_state_dict_semantic_encoder[new_key] = value
+        elif key.startswith('fc_prior.'):
+            # 去掉 'generator.' 前缀，以匹配 CodecDecoder 的参数名
+            new_key = key[len('fc_prior.'):]
+            filtered_state_dict_fc_prior[new_key] = value
+    semantic_model = Wav2Vec2BertModel.from_pretrained("facebook/w2v-bert-2.0", output_hidden_states=True)
+
+    semantic_model.eval().cuda()
+    SemanticEncoder_module = SemanticEncoder(1024,1024,1024)
+    SemanticEncoder_module.load_state_dict(filtered_state_dict_semantic_encoder)
+    SemanticEncoder_module = SemanticEncoder_module.eval().cuda()
+    encoder = CodecEncoder_Transformer()
+    encoder.load_state_dict(filtered_state_dict_codec)
+    encoder = encoder.eval().cuda()
+    decoder = CodecDecoderVocos()
+    decoder.load_state_dict(filtered_state_dict_gen)
+    decoder = decoder.eval().cuda()
+    fc_post_a = nn.Linear( 2048, 1024 )
+    fc_post_a.load_state_dict(filtered_state_dict_fc_post_a)
+    fc_post_a = fc_post_a.eval().cuda()
+    fc_prior = nn.Linear( 2048, 2048 )
+    fc_prior.load_state_dict(filtered_state_dict_fc_prior)
+    fc_prior = fc_prior.eval().cuda()
+    feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/w2v-bert-2.0")
+
+    wav_dir = args.output_dir
+    os.makedirs(wav_dir, exist_ok=True)
+
+    
+    
+    # wav_paths = glob(join(args.input_dir, '*.flac')) #
+    # both wav and flac and mp3
+    wav_paths = glob(os.path.join(args.input_dir, '**', '*.wav'), recursive=True)
+    flac_paths = glob(os.path.join(args.input_dir, '**', '*.flac'), recursive=True)
+    mp3_paths = glob(os.path.join(args.input_dir, '**', '*.mp3'), recursive=True)
+
+    # 合并所有路径
+    wav_paths = wav_paths + flac_paths + mp3_paths
+    print(f'Found {len(wav_paths)} wavs in {args.input_dir}')
+    
+    st = time()
+    for wav_path in tqdm(wav_paths):
+        target_wav_path = join(wav_dir, basename(wav_path))
+        wav = librosa.load(wav_path, sr=sr)[0] 
+        wav_cpu = torch.from_numpy(wav)
+
+        feat =  feature_extractor(F.pad(wav_cpu, (160, 160)), sampling_rate=16000, return_tensors="pt") .data['input_features']
+
+        feat = feat.cuda()
+        wav = wav_cpu.unsqueeze(0).cuda()
+        wav = torch.nn.functional.pad(wav, (0, (200 - (wav.shape[1] % 200))))
+        with torch.no_grad():
+            vq_emb = encoder(wav.unsqueeze(1))
+            vq_emb = vq_emb.transpose(1, 2)
+
+            if vq_emb.shape[2]!=feat.shape[1]:
+                feat =  feature_extractor(wav_cpu, sampling_rate=16000, return_tensors="pt") .data['input_features']
+
+                feat = feat.cuda()
+
+            semantic_target = semantic_model(feat[:,  :,:])
+
+            semantic_target = semantic_target.hidden_states[16]
+
+            semantic_target = semantic_target.transpose(1, 2)
+            semantic_target = SemanticEncoder_module(semantic_target)
+             
+
+
+            vq_emb = torch.cat([semantic_target, vq_emb], dim=1)
+            vq_emb =  fc_prior(vq_emb.transpose(1, 2)).transpose(1, 2)
+
+            _, vq_code, _ = decoder(vq_emb, vq=True)  # vq_code here !!!!
+
+            vq_post_emb = decoder.quantizer.get_output_from_indices(vq_code.transpose(1, 2))
+            vq_post_emb = vq_post_emb.transpose(1, 2)
+            vq_post_emb = fc_post_a(vq_post_emb.transpose(1,2)).transpose(1,2)
+            recon = decoder(vq_post_emb.transpose(1, 2), vq=False)[0].squeeze().detach().cpu().numpy()
+            # recon = decoder(decoder.vq2emb(vq_code.transpose(1,2)).transpose(1,2), vq=False).squeeze().detach().cpu().numpy()
+        sf.write(target_wav_path, recon, sr)
+    et = time()
+    print(f'Inference ends, time: {(et-st)/60:.2f} mins')
+
+
+
+
+
